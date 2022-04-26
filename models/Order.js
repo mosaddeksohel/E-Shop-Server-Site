4f5b4ad1a83140067a4d606be2a840e8cf9d6855
Sohel <tanjim.sohel885@gmail.com>
2022-04-26 20:10:40 +0600

    setup order

--- models/Order.js
+++ models/Order.js
@@ -19,6 +19,7 @@ const OrderSchema = new mongoose.Schema(
     ],
     amount: { type: Number, required: true },
     address: { type: Object, required: true },
+    status: { type: String, default: "pending" },
   },
   { timestamps: true }
 );
