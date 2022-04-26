a5f4f7bffc6f55442e004f4ec15cf49108e9485d
Sohel <tanjim.sohel885@gmail.com>
2022-04-26 20:39:12 +0600

    setup user

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const port = process.env.PORT || 5000;
 
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const userRoute = require("./routes/user");
 const cors = require("cors");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
@@ -24,6 +25,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(port, () =>
   console.log(`Backend Server is running on port ${port}!`)
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
 
-router.get("/usertest", (req, res) => {
+/* router.get("/usertest", (req, res) => {
   res.send("user test is suffessful");
 });
 
 router.post("/userposttest", (req, res) => {
   console.username = req.body.username;
   res.send("Your username is" + username);
-});
+}); */
 
 module.exports = router;
