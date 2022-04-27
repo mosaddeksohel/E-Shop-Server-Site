5496627fbbcd33f347514a277a70ffb37ee63886
Sohel <tanjim.sohel885@gmail.com>
2022-04-27 12:18:56 +0600

    setup login & secure user

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 
 const User = require("../models/User");
+const CryptoJS = require("crypto-js");
 
 // REGISTER
 
@@ -8,12 +9,36 @@ router.post("/register", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString(),
   });
 
   try {
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(401).json("Wrong Credentials");
+
+    const hashPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    );
+    const OriginalPassword = hashPassword.toString(CryptoJS.enc.Utf8);
+
+    OriginalPassword !== req.body.password &&
+      res.status(401).json("Wrong Credentials");
+
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
   } catch (err) {
     res.status(500).json(err);
   }
